3c1de4cea9261522e4a3389e8a763810d0142837
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-19 15:09:46 +0200

    Set main colors & fonts

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ThreadComponent } from './thread/thread.component';
+import { ChannelComponent } from './channel/channel.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ThreadComponent, ChannelComponent, WorkspaceComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
