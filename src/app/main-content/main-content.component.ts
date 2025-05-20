c69a3177aaef909565631c5858e70f6cb3f95c10
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-20 10:42:45 +0200

    Added menu-toggle service, working on main-component(header, workspace toggle function)

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ThreadComponent } from './thread/thread.component';
 import { ChannelComponent } from './channel/channel.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
+import { MenuToggleService } from '../services/menu-toggle.service';
 
 @Component({
   selector: 'app-main-content',
@@ -12,5 +13,23 @@ import { WorkspaceComponent } from './workspace/workspace.component';
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
+
+  toggleMenu = inject(MenuToggleService);
+  workspaceOpened: boolean = false;
+  workspaceHovered: boolean = false;
+  workspaceStatus:  "Open" | "Close" = "Open";
+
+
+  toggleWorkspaceMenu():void {
+    this.workspaceOpened = !this.workspaceOpened;
+    this.workspaceStatus = this.workspaceOpened ? 'Close' : 'Open';
+    this.workspaceOpened ? this.toggleMenu.showWorkspace.set(true) : this.toggleMenu.showWorkspace.set(false);
+  }
+
+  getWorkspaceIcon(): string {
+  const color = this.workspaceHovered ? 'blue' : 'black';
+  const status = this.workspaceStatus === 'Open' ? 'workspace_open' : 'workspace_close';
+  return `./../../assets/icons/menu/${status}_${color}.svg`;
+}
 
 }
