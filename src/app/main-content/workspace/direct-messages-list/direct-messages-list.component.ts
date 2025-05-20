3ca3ee004f2f97bdc2e37226317cdd47c1f0cb9e
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-20 16:32:32 +0200

    Working on drop-down-lists channel & direct-message

--- src/app/main-content/workspace/direct-messages-list/direct-messages-list.component.ts
+++ src/app/main-content/workspace/direct-messages-list/direct-messages-list.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './direct-messages-list.component.scss'
 })
 export class DirectMessagesListComponent {
+  tempArrayItemCount: string[] = ['fem_1', 'fem_2', 'mal_1', 'mal_2', 'mal_3'];
+  channelListOpened: boolean = false;
 
+  toggleChannelList() {
+    this.channelListOpened = !this.channelListOpened;
+  }
 }
