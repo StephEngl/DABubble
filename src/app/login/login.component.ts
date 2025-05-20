dc91675709631ef6f7767a6041c0e2f190fe5152
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-20 16:50:56 +0200

    Implement login dialog component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LoginDialogComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
