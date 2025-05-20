bdc8fcbf4c0fa40fdda5eae216e6d7655ea08f03
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-05-20 14:55:18 +0200

    Moved legal-notice and privacy-policy.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './services/auth-guard.guard';
 import { MainContentComponent } from './main-content/main-content.component';
-import { LegalNoticeComponent } from './login/legal-notice/legal-notice.component';
-import { PrivacyPolicyComponent } from './login/privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
