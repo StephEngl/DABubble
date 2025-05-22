6d55be4e4a0ac87630fe96c515f9a542efe46e9e
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-22 17:22:59 +0200

    Add forgot password dialog content

--- src/app/login/password-dialog/password-dialog.component.ts
+++ src/app/login/password-dialog/password-dialog.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { SignalsService } from '../../services/signals.service';
 
 @Component({
   selector: 'app-password-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './password-dialog.component.html',
-  styleUrl: './password-dialog.component.scss'
+  styleUrl: './password-dialog.component.scss',
 })
 export class PasswordDialogComponent {
+  signalService = inject(SignalsService);
+  emailInput: string = '';
 
+  /**
+   * Called when the login form is submitted.
+   * Sets the submission flag if the login is not a guest login.
+   * @param ngForm - The Angular form instance.
+   */
+  onSubmit(ngForm: NgForm) {}
+
+  backToLogin() {
+    this.signalService.isLoginDialog.set(true);
+    this.signalService.isRegisterDialog.set(false);
+    this.signalService.isChoosingAvatarDialog.set(false);
+    this.signalService.isPasswordForgottenDialog.set(false);
+  }
+
+  sendMailForNewPassword(email: string) {
+    // send mail to reset password
+    this.backToLogin();
+  }
 }
