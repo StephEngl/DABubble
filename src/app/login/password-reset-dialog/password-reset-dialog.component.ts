141ae279bd8426bcb3f5e0226daf182828ed4370
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-28 15:31:27 +0200

    Add input fields to forgot password dialog

--- src/app/login/password-reset-dialog/password-reset-dialog.component.ts
+++ src/app/login/password-reset-dialog/password-reset-dialog.component.ts
@@ -1,14 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { SignalsService } from '../../services/signals.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-password-reset-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './password-reset-dialog.component.html',
-  styleUrl: './password-reset-dialog.component.scss'
+  styleUrl: './password-reset-dialog.component.scss',
 })
 export class PasswordResetDialogComponent {
-signalService = inject(SignalsService);
+  authService = inject(AuthenticationService);
+  signalService = inject(SignalsService);
+
+  formSubmitted = false;
+  passwordVisible: Boolean = false;
+  confirmPasswordVisible: Boolean = false;
+  passwordInput: string = '';
+  confirmPasswordInput: string = '';
+
+  onSubmit(ngForm: NgForm) {
+
+  }
+
+  setNewPassword(password: string) {
+
+  }
+
+  /** Toggles the visibility of the password input field. */
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  /** Toggles the visibility of the confirmation password input field. */
+  toggleConfirmPasswordVisibility() {
+    this.confirmPasswordVisible = !this.confirmPasswordVisible;
+  }
 
 }
