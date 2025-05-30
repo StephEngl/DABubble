d424a9dd17c835ad82ef207bbf1dd63a0064abbb
Stephanie Englberger <stephaniekoebich@aol.com>
2025-05-30 16:41:43 +0200

    Add form validation to password reset dialog

--- src/app/login/password-reset-dialog/password-reset-dialog.component.ts
+++ src/app/login/password-reset-dialog/password-reset-dialog.component.ts
@@ -20,12 +20,16 @@ export class PasswordResetDialogComponent {
   passwordInput: string = '';
   confirmPasswordInput: string = '';
 
-  onSubmit(ngForm: NgForm) {
+  onSubmit(ngForm: NgForm) {}
 
-  }
+  setNewPassword(password: string) {}
 
-  setNewPassword(password: string) {
-
+  get passwordsMatch(): boolean {
+    if (this.passwordInput) {
+      return (this.passwordInput === this.confirmPasswordInput);
+    } else {
+      return false;
+    }
   }
 
   /** Toggles the visibility of the password input field. */
@@ -37,5 +41,4 @@ export class PasswordResetDialogComponent {
   toggleConfirmPasswordVisibility() {
     this.confirmPasswordVisible = !this.confirmPasswordVisible;
   }
-
 }
