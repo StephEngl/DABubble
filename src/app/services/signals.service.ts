4381e5f66428467689c7303f53cae0f6c0830f08
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-06-03 18:04:07 +0200

    Hide add-member dialog on app init

--- src/app/services/signals.service.ts
+++ src/app/services/signals.service.ts
@@ -24,7 +24,7 @@ export class SignalsService {
   showCreateChannel = signal<boolean>(false);
   showChannelMembers = signal<boolean>(false);
   showUserInfo = signal<boolean>(false);
-  showAddMembers = signal<boolean>(true);
+  showAddMembers = signal<boolean>(false);
 
   scrollChannelToBottom = signal<boolean>(false);
 
