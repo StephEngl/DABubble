3821371e9b18a568883d27cac98d1300aad14772
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-06-03 17:48:44 +0200

    Added search and filter function to add-members-dialog

--- src/app/main-content/channel/add-members/add-members.component.ts
+++ src/app/main-content/channel/add-members/add-members.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { SignalsService } from '../../../services/signals.service';
 import { ChannelsService } from '../../../services/channels.service';
+import { FormsModule } from '@angular/forms';
+import { UsersService } from '../../../services/users.service';
+import { UserInterface } from '../../../interfaces/user.interface';
+
 
 @Component({
   selector: 'app-add-members',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-members.component.html',
   styleUrl: './add-members.component.scss'
 })
@@ -13,13 +17,15 @@ export class AddMembersComponent {
 
   signalService = inject(SignalsService);
   channelService = inject(ChannelsService);
+  userService = inject(UsersService);
 
   addAllMembers: boolean = false;
   addSpecificMembers: boolean = false;
   userSelectionActive: boolean = false;
   allMembersCheckStatus: string = './../../../../assets/icons/menu/circle_blue.svg';
   specificMembersCheckStatus: string = './../../../../assets/icons/menu/circle_blue.svg';
-  selectedMembers: string[] = ['2'];
+  selectedMembers: UserInterface[] = [];
+  textInput: string = '';
 
 
   toggleAddAllMembers():void {
@@ -52,7 +58,33 @@ export class AddMembersComponent {
   }
 
   removeMember(id:string) {
+    this.selectedMembers = this.selectedMembers.filter(m => m.id !== id);
+  }
 
+  get searchResultsUser() {
+    const searchTerm = this.textInput.trim().toLowerCase();
+    if (!searchTerm) return [];
+
+    const currentChannelId = localStorage.getItem('currentChannel');
+    const currentChannel = this.channelService.getChannelById(currentChannelId!);
+    const currentMembers = currentChannel!.members!;
+
+    const selectedIds = this.selectedMembers.map((m) => m.id);
+
+    return this.userService.users
+      .filter(user =>
+        user.name.toLowerCase().includes(searchTerm) &&
+        !currentMembers.includes(user.id!) &&
+        !selectedIds.includes(user.id!)
+      );
+  }
+
+  addMember(member: UserInterface):void {
+    const alreadyExists = this.selectedMembers.find((m) => m === member);
+    if (!alreadyExists) {
+      this.selectedMembers.push(member);
+      this.textInput= '';
+    }
   }
 
 }
