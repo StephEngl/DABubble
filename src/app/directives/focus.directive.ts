cd3e03fa0780ccecd7fc1f3a5a3f74471dead6b3
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-06-06 11:12:35 +0200

    Added directive (focusing dom element)

---
+++ src/app/directives/focus.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[appFocus]',
+  standalone: true
+})
+export class FocusDirective implements OnChanges {
+
+  @Input() appFocus = false;
+
+  element: HTMLElement;
+
+  constructor(ref: ElementRef) {
+    this.element = ref.nativeElement;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const change = changes['appFocus'];
+
+    if (change && change.currentValue === true) {
+      this.element.focus();
+    }
+  }
+}
--- src/app/main-content/channel/channel-info/channel-info.component.ts
+++ src/app/main-content/channel/channel-info/channel-info.component.ts
@@ -1,18 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { SignalsService } from '../../../services/signals.service';
+import { FocusDirective } from '../../../directives/focus.directive';
 
 @Component({
   selector: 'app-channel-info',
   standalone: true,
-  imports: [],
+  imports: [FocusDirective],
   templateUrl: './channel-info.component.html',
   styleUrl: './channel-info.component.scss'
 })
+
 export class ChannelInfoComponent {
   
   signalService = inject(SignalsService);
 
   channelNameEdit: boolean = false;
   channelDescriptionEdit: boolean = false;
-
+  @ViewChild('channelDescription') textarea!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('channelName') input!: ElementRef<HTMLInputElement>;
+  
 }
