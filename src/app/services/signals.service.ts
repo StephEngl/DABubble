f5323bdc274813fcba80b289689c4e9abc822e9c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-06-06 10:50:23 +0200

    Renaming signal for channel-info

--- src/app/services/signals.service.ts
+++ src/app/services/signals.service.ts
@@ -23,7 +23,7 @@ export class SignalsService {
   showThread = signal<boolean>(false);
   showChannel = signal<boolean>(true);
   showCreateChannel = signal<boolean>(false);
-  showEditChannel = signal<boolean>(true);
+  showChannelInfo = signal<boolean>(true);
   showChannelMembers = signal<boolean>(false);
   showUserInfo = signal<boolean>(false);
   showAddMembers = signal<boolean>(false);
