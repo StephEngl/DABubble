abc532b94b72d369fbe202d61914113f9da11a0d
Stephanie Englberger <stephaniekoebich@aol.com>
2025-06-06 15:19:07 +0200

    Add legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SignalsService } from '../services/signals.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
-
+signalService = inject(SignalsService)
 }
