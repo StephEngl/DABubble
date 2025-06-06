7ec5ed52d8338d95f90f06a606166c2bca87d487
Stephanie Englberger <stephaniekoebich@aol.com>
2025-06-06 21:25:16 +0200

    Add RouterLink to go back to login to privacy policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { SignalsService } from '../services/signals.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
