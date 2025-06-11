b187aa1f6e864f9fe8856c5522ab36863acca4d7
Stephanie Englberger <stephaniekoebich@aol.com>
2025-06-11 13:56:03 +0200

    Fix password reset section didn't work

--- src/app/login/password-reset-dialog/password-reset-dialog.component.ts
+++ src/app/login/password-reset-dialog/password-reset-dialog.component.ts
@@ -1,9 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { SignalsService } from '../../services/signals.service';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
-import { UsersService } from '../../services/users.service';
-import { UserInterface } from '../../interfaces/user.interface';
 import { ActivatedRoute } from '@angular/router';
 import { PasswordService } from '../../services/password.service';
 import { PasswordsComponent } from '../passwords/passwords.component';
@@ -18,19 +16,11 @@ import { PasswordsComponent } from '../passwords/passwords.component';
 export class PasswordResetDialogComponent {
   authService = inject(AuthenticationService);
   signalService = inject(SignalsService);
-  userService = inject(UsersService);
   passwordService = inject(PasswordService);
-
-  passwordVisible: boolean = false;
-  confirmPasswordVisible: boolean = false;
-  passwordInput: string = '';
-  confirmPasswordInput: string = '';
 
   oobCode: string = '';
   email: string = '';
   error: string = '';
-
-  currentUser?: UserInterface;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -40,6 +30,7 @@ export class PasswordResetDialogComponent {
    */
   async ngOnInit() {
     this.extractOobCodeOutOfURL();
+    this.signalService.passwordsMatch.set(false);
   }
 
   /**
@@ -63,28 +54,10 @@ export class PasswordResetDialogComponent {
   }
 
   /**
-   * Checks whether the password and confirmation password inputs match.
-   * @returns True if both passwords match and are not empty; otherwise, false.
-   */
-  // get passwordsMatch(): boolean {
-  //   if (this.passwordInput) {
-  //     return this.passwordInput === this.confirmPasswordInput;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
-  /**
    * Handles form submission: validates input and handles errors.
    */
   async setNewPassword() {
-    if (!this.signalService.passwordsMatch || !this.oobCode) return;
-    this.passwordService.setNewPassword(this.oobCode, this.confirmPasswordInput);
-    // try {
-    //   await this.resetPassword(this.oobCode, this.passwordInput);
-    //   this.handlePasswordResetSuccess();
-    // } catch (err: any) {
-    //   this.handlePasswordResetError(err);
-    // }
+    if (!this.signalService.passwordsMatch() || !this.oobCode) return;
+    this.passwordService.setNewPassword(this.oobCode, this.signalService.confirmPasswordInput());
   }
 }
